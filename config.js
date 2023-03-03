b7285cf6ac1bbafaac8843cc9abfb2e06ba445b7
Hieusenpai <106915167+Hieusenpai@users.noreply.github.com>
2023-03-03 22:46:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Muốn viết tặng cho em
-Một bài thơ nào ấy.
-Nhưng nghĩ hoài chỉ thấy
-Một từ nhớ em thôi thơ hay chưa :V.`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Nhung cuti khum đáng sợ!",
+    introTitle: 'Nhung cutie khum đáng sợ!',
+    introDesc: `Thức tới sáng vì quá chán. Ngủ muộn màng là vì nàng , thì ra mấy nay teo ngủ muộn là vì nhung à:V.`,
+    btnIntro: 'bấm vào đây đi bé ơi',
+    title: 'chả biết tặng mi cái gì nhung ạ 🥰',
+    desc: 'thôi thì tặng đoạn code này, mà có thích uống trà sữa khum có thì chọn đi ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    question: 'cứ tưởng bé không thích mà :V sao lại đổi ý rồi, mún nói gì thì nhắn vào đi ở dưới đó<3',
+    btnReply: 'ghi xong thì bấm vào đây nhé Nhung cuti<3',
+    reply: 'bé mún uống trà sữa híu ơi <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘 nhưng mà có gặp đâu mà tà tữa',
+    messDesc: 'bao h gặp thì híu bao nhe , h chỉ tặng mỗi tấm lòng nì nè lấy khum.',
+    btnAccept: 'nhấn vào đây để tới mess của híu <3',
     messLink: 'https://www.facebook.com/messages/t/100047365355499' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
